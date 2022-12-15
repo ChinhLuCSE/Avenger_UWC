0e4bcb270dcf62656c580501e1ac017454ff2f7d
ChinhLuCSE <100012219+ChinhLuCSE@users.noreply.github.com>
2022-12-15 16:54:42 +0700

    update login va forgotpassword
    khong hien thi anh ben trang forgotpassword

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ import Header from './components/header/Header'
 import Navbar from './components/navbar/Navbar'
 import HomeTask from './components/task/homeTask/HomeTask'
 import TaskDetail from './components/task/taskDetail/TaskDetail'
-import ModifyTask from './components/task/modifyTask/ModifyTask';
+import ModifyTask from './components/task/modifyTask/ModifyTask'
+import Login from './components/signin/login/Login'
+import ForgotPassword from './components/signin/forgot_password/ForgotPassword'
+;
+
 
 function App() {
   return (
@@ -19,6 +23,8 @@ function App() {
                   <Route path="/task" element={<HomeTask/>} />
                   <Route path="/task/detail" element={<TaskDetail/>} />
                   <Route path="/task/modify" element={<ModifyTask/>} />
+                  <Route path="/login" element={<Login/>} />
+                  <Route path="/login/forgotpw" element={<ForgotPassword/>} />
                 </Routes>
                 <Footer/>
             </div> 
